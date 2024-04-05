6f900aaad6b8560e3d70b7134c1db62c60df1f0a
Malte Lau Petersen <maltelau@mlpetersen.dk>
2024-04-05 15:06:07 +0200

    add other models + start plots

--- portfolio3/portfolio3.R
+++ portfolio3/portfolio3.R
@@ -42,6 +42,10 @@ sim_weighted_betabinomial <- function(df, lb = 1, ub = 8, invTemperature = 1, lo
 ## compile models
 simple_betabayes <- cmdstan_model("portfolio3/betabinomial-simple-single.stan")
 weighted_betabayes <- cmdstan_model("portfolio3/betabinomial-weighted-single.stan")
+
+simple_bayes <- cmdstan_model("portfolio3/simple_Bayes_model.stan")
+weighted_bayes <- cmdstan_model("portfolio3/weighted_Bayes_model.stan")
+
 #weighted_temp_betabayes <- cmdstan_model("portfolio3/betabinomial-weighted-single-temp.stan")
 
 fit_betabayes <- function(model, data, lb = 1, ub = 8, fixed_param=FALSE) {
@@ -58,6 +62,20 @@ fit_betabayes <- function(model, data, lb = 1, ub = 8, fixed_param=FALSE) {
         adapt_delta=.95,
         fixed_param=fixed_param
     )
+}
+
+fit_bayes <- function(model, data) {
+    model$sample(
+        data = list(
+            trials = nrow(data),
+            FirstRating = data$FirstRating,
+            GroupRating = data$GroupRating,
+            SecondRating = data$SecondRating
+        ),
+        parallel_chains = 4,
+        adapt_delta=.95
+    )
+
 }
 
 ## ## simulate data from the model to assess whether the models can be fit.
@@ -93,25 +111,50 @@ fit_betabayes <- function(model, data, lb = 1, ub = 8, fixed_param=FALSE) {
 
 
 x <- data_patients |>
-  head(10) |>
+  head(200) |>
   group_by(ID) |>
   group_modify(function(data, participant) {
     m1 <- fit_betabayes(simple_betabayes, data, fixed_param = TRUE)
     m2 <- fit_betabayes(weighted_betabayes, data)
+    m3 <- fit_bayes(simple_bayes, data)
+    m4 <- fit_bayes(weighted_bayes, data)
     distinct(data, Condition) |>
-      mutate(loo = list(loo_compare(m1$loo(), m2$loo())),
-             yrep = list(draws_df(m1, "y_rep")))
+      mutate(loo   = list(rownames_to_column(as_tibble(loo_compare(m1$loo(), m2$loo(), m3$loo(), m4$loo()), rownames = NA))),
+             y_rep = list(as_draws_df(m1$draws("y_rep"))))
   })
 
+## model comparison result, by ID
+unnest(x, loo) |>
+  mutate(ymin = elpd_diff - 2 * se_diff,
+         ymax = elpd_diff + 2  *se_diff) |>
+  mutate(top_model = rowname[])
+  ggplot(aes(ID, elpd_diff, ymin=ymin, ymax=ymax, color=rowname)) +
+  geom_pointrange()
+
+
+unnest(x, loo) |>
+  filter(elpd_diff == 0) |>
+  ggplot(aes(rowname)) +
+  geom_histogram(stat="count")
+
+
+## posterior predictive check
+y <- head(data_patients, 200)$SecondRating
+
+yrep <- unnest(x, y_rep) |>
+  ungroup() |>
+  select(-ID, -Condition, yrep-loo)
+
+yrep_groups <- unnest(x, y_rep) |>
+  ungroup() |>
+  select(ID)
+
+
+ppc_dens_overlay_grouped(y, as.matrix(yrep), yrep_groups)
 
 
 
-### fit them to one dataset (don't forget to explore the data first!)
-## m1 <- fit_gumball(simple_betabayes, one_participant, fixed_param=TRUE)
-## m2 <- fit_gumball(weighted_betabayes, one_participant)
-## m3 <- fit_gumball(weighted_temp_betabayes, one_participant)
-loo_compare(m1$loo(), m2$loo(), m3$loo())
-
+## old code
 posterior <- m2$draws(variable = c("log_weight_mu", "log_weight_delta"))
 
 mcmc_trace(posterior)
